887a055314be97b8ff06ccb031823f092a8e60a7
fob413 <oluwafunso.oluyole.balogun@gmail.com>
2024-05-16 20:28:59 -0300

    Layout Setup - installed ant design for uitilize their components - created the basic layout for the application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import { Layout } from "antd";
+import Drawer from "./components/drawer";
 import './App.css'
 
 function App() {
   return (
-    <>
+    <Layout>
+        <Drawer />
+
         <h1>Geo Location Search</h1>
-    </>
+    </Layout>
   )
 }
 
